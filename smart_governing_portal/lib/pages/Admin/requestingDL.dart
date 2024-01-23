0666a88536e3268c8db587b0e2440728016a17b2
Sanuda3088 <sanudasenod446@gmail.com>
2024-01-23 23:49:22 +0530

    dl done

--- smart_governing_portal/lib/pages/Admin/requestingDL.dart
+++ smart_governing_portal/lib/pages/Admin/requestingDL.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 import 'package:smart_governing_portal/pages/Admin/requestingDLTemplate.dart';
 
-
 class RequestingDL extends StatefulWidget {
   @override
   _RequestingDLState createState() => _RequestingDLState();
@@ -25,110 +24,112 @@ class _RequestingDLState extends State<RequestingDL> {
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(20.0),
-            child: StreamBuilder(
-              stream: dlTestCollection.snapshots(),
-              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-            
-                if (snapshot.hasError) {
-                  return Center(
-                    child: Text('Error: ${snapshot.error}'),
-                  );
-                }
-            
-                List<TableRow> rows = snapshot.data!.docs.map((doc) {
-                  String name = doc['Full Name'] ?? 'No Name';
-                  String nic = doc['License No'] ?? 'No Name';
-                  Timestamp timestamp = doc['Time'] ?? Timestamp(0, 0);
-                  String formattedTime =
-                      DateFormat('yyyy-MM-dd HH:mm').format(timestamp.toDate());
-            
-                  return TableRow(
-                    children: [
-                      TableCell(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(nic),
-                        ),
-                      ),
-                      TableCell(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(name),
-                        ),
-                      ),
-                      TableCell(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(formattedTime),
-                        ),
-                      ),
-                      TableCell(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => ReqDLTemplate(documentId: doc.id,),
-                                ),
-                              );
-                            },
-                            child: const Text("View"),
-                          ),
-                        ),
-                      ),
-                    ],
-                  );
-                }).toList();
-            
-                return Table(
-                  defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                  border: TableBorder.all(color: const Color.fromARGB(255, 49, 101, 185)),
-                  columnWidths: const <int, TableColumnWidth>{
-                    0: IntrinsicColumnWidth(),
-                    1: IntrinsicColumnWidth(),
-                    2: IntrinsicColumnWidth(),
-                    3: IntrinsicColumnWidth(),
-                  },
-                  children: [
-                    const TableRow(
+            child: InteractiveViewer(
+              child: StreamBuilder(
+                stream: dlTestCollection.snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+              
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text('Error: ${snapshot.error}'),
+                    );
+                  }
+              
+                  List<TableRow> rows = snapshot.data!.docs.map((doc) {
+                    String name = doc['Full Name'] ?? 'No Name';
+                    String nic = doc['License No'] ?? 'No Name';
+                    Timestamp timestamp = doc['Time'] ?? Timestamp(0, 0);
+                    String formattedTime =
+                        DateFormat('yyyy-MM-dd HH:mm').format(timestamp.toDate());
+              
+                    return TableRow(
                       children: [
                         TableCell(
                           child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text('License No',style: TextStyle(color:Color.fromARGB(255, 49, 101, 185),fontSize: 18,fontWeight: FontWeight.w600 ),),
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(nic),
                           ),
                         ),
                         TableCell(
                           child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text('Name',style: TextStyle(color:Color.fromARGB(255, 49, 101, 185),fontSize: 18,fontWeight: FontWeight.w600 ),),
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(name),
                           ),
                         ),
                         TableCell(
                           child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text('Time',style: TextStyle(color:Color.fromARGB(255, 49, 101, 185),fontSize: 18,fontWeight: FontWeight.w600 ),),
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(formattedTime),
                           ),
                         ),
                         TableCell(
                           child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text(''),
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => ReqDLTemplate(documentId: doc.id,),
+                                  ),
+                                );
+                              },
+                              child: const Text("View"),
+                            ),
                           ),
                         ),
                       ],
-                    ),
-                    ...rows,
-                  ],
-                );
-              },
+                    );
+                  }).toList();
+              
+                  return Table(
+                    defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                    border: TableBorder.all(color: const Color.fromARGB(255, 49, 101, 185)),
+                    columnWidths: const <int, TableColumnWidth>{
+                      0: IntrinsicColumnWidth(),
+                      1: IntrinsicColumnWidth(),
+                      2: IntrinsicColumnWidth(),
+                      3: IntrinsicColumnWidth(),
+                    },
+                    children: [
+                      const TableRow(
+                        children: [
+                          TableCell(
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text('License No',style: TextStyle(color:Color.fromARGB(255, 49, 101, 185),fontSize: 18,fontWeight: FontWeight.w600 ),),
+                            ),
+                          ),
+                          TableCell(
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text('Name',style: TextStyle(color:Color.fromARGB(255, 49, 101, 185),fontSize: 18,fontWeight: FontWeight.w600 ),),
+                            ),
+                          ),
+                          TableCell(
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text('Time',style: TextStyle(color:Color.fromARGB(255, 49, 101, 185),fontSize: 18,fontWeight: FontWeight.w600 ),),
+                            ),
+                          ),
+                          TableCell(
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(''),
+                            ),
+                          ),
+                        ],
+                      ),
+                      ...rows,
+                    ],
+                  );
+                },
+              ),
             ),
           ),
         ),
