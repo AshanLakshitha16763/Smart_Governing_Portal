80deb0c532ccb81d22407464f7e27144f44a6964
Isuru <isurumbandara@gmail.com>
2024-02-01 20:56:29 +0530

    User login Animation pages images add

--- smart_governing_portal/lib/main.dart
+++ smart_governing_portal/lib/main.dart
@@ -4,22 +4,20 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:lottie/lottie.dart';
 import 'package:smart_governing_portal/controllers/chat.dart';
-import 'package:smart_governing_portal/controllers/secrets.dart';
+//import 'package:smart_governing_portal/controllers/secrets.dart';
 import 'package:smart_governing_portal/controllers/widget_tree.dart';
-
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   if (kIsWeb) {
     await Firebase.initializeApp(
-      options:  FirebaseOptions(
-        apiKey:  apikey, 
-        appId: appid, 
-        messagingSenderId: messagingsenderid, 
-        projectId: projectid,
-        storageBucket: storagebucket
-      )
-    );
+        options: FirebaseOptions(
+      apiKey: "AIzaSyAQGH-OoQz5jHZKxMR6t6GXXuQLo82RF6M",
+      appId: "1:913759630420:web:9fac7bd0a1c5cec94d69c1",
+      messagingSenderId: "913759630420",
+      projectId: "smart-governing-portal",
+      storageBucket: "smart-governing-portal.appspot.com",
+    ));
   }
   await Firebase.initializeApp();
 
@@ -41,7 +39,7 @@ class _MyAppState extends State<MyApp> {
     super.initState();
 
     // Simulate a 5-second delay and then update loading state
-    Timer(const Duration(seconds:4), () {
+    Timer(const Duration(seconds: 4), () {
       setState(() {
         _isLoading = false;
       });
@@ -54,14 +52,18 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       title: 'Smart Governing Portal',
       theme: ThemeData(
-          fontFamily: 'Poppins',
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 205, 219, 243)),
-          useMaterial3: true,
-        ),
-      home: _isLoading ? Center(child: Lottie.asset('lib/Assets/animations/loading.json',frameRate: FrameRate(40)),) : const WidgetTree(),
+        fontFamily: 'Poppins',
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 205, 219, 243)),
+        useMaterial3: true,
+      ),
+      home: _isLoading
+          ? Center(
+              child: Lottie.asset('lib/Assets/animations/loading.json',
+                  frameRate: FrameRate(40)),
+            )
+          : const WidgetTree(),
     );
   }
 }
-
